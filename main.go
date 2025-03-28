c4bfa7a6b6be80e0c8bb442d42e827845beb573b
Pramananda Sarkar <pramanandasarkar02@gmail.com>
2025-03-28 16:41:29 +0600

    simple struct done

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"encoding/xml"
+	"fmt"	
+	"io"
+	"log"
+
 	"net/http"
 	"strconv"
 	"sync"
 
 	"github.com/gorilla/mux"
-	"golang.org/x/net/websocket"
+	"github.com/gorilla/websocket"
+	"github.com/graphql-go/graphql"
 )
 
 
@@ -139,6 +144,117 @@ func restUpdatePost(w http.ResponseWriter, r *http.Request){
 }
 
 
+func restDeletePost(w http.ResponseWriter, r *http.Request){
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	del := deletePost(id)
+	if del {
+		msg := fmt.Sprint(id)+ " id deleted"
+		json.NewEncoder(w).Encode(msg)
+		return 
+	}
+	http.Error(w, "can't deleted the post", http.StatusNotFound)
+}
+
+
+
+
+type SoapRequest struct {
+	XMLName xml.Name `xml:"CreatePost"`
+	Title string `xml:"title"`
+	Content string `xml:"content"`
+}
+
+
+type SoapResponse struct {
+	XMLName xml.Name `xml:"CreatePostResponse"`
+	Post Post 	`xml:"post"`
+}
+
+func soapHandler(w http.ResponseWriter, r *http.Request){
+	body, _ := io.ReadAll(r.Body)
+	var req SoapRequest
+	xml.Unmarshal(body, &req)
+	p := createPost(req.Title, req.Content)
+	resp := SoapResponse{Post: p}
+	w.Header().Set("Content-Type", "text/xml")
+	xml.NewEncoder(w).Encode(resp)
+}
+
+
+
+
+
+
+// graphql
+
+
+var postType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Post",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{Type: graphql.Int},
+		"title": &graphql.Field{Type: graphql.String},
+		"content": &graphql.Field{Type: graphql.String},
+
+	},
+})
+
+
+
+var queryType =  graphql.NewObject(graphql.ObjectConfig{
+	Name: "Query",
+	Fields: graphql.Fields{
+		"posts": &graphql.Field{
+			Type: graphql.NewList(postType),
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return listPosts(), nil
+			},
+		},
+	},
+})
+
+
+var mutationType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Mutation",
+	Fields: graphql.Fields{
+		"createPost": &graphql.Field{
+			Type: postType,
+			Args: graphql.FieldConfigArgument{
+				"title": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+				"content": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+				
+			},
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return createPost(p.Args["title"].(string), p.Args["content"].(string)), nil
+			},
+		},
+	},
+})
+
+
+var schema, _ = graphql.NewSchema(graphql.SchemaConfig{
+	Query: queryType,
+	Mutation: mutationType,
+})
+
+
+func graphqlHandler(w http.ResponseWriter, r * http.Request){
+	var params struct{
+		Query string 
+	}
+	json.NewDecoder(r.Body).Decode(&params)
+	result := graphql.Do(graphql.Params{
+		Schema: schema,
+		RequestString: params.Query,
+	})
+	json.NewEncoder(w).Encode(result)
+}
+
+
 
 
 
@@ -149,27 +265,110 @@ func restServer(){
 	r.HandleFunc("/api/posts", restListPosts).Methods("GET")
 	r.HandleFunc("/api/posts/{id}", restGetPost).Methods("GET")
 	r.HandleFunc("/api/posts/{id}", restUpdatePost).Methods("PUT")
+	r.HandleFunc("/api/posts/{id}", restDeletePost).Methods("DELETE")
 	r.HandleFunc("/api/posts", restCreatePost).Methods("POST")
 	go http.ListenAndServe(":8080", r)
 
 }
 
 
+
+
+func soapServer(){
+	http.HandleFunc("/soap", soapHandler)
+	go http.ListenAndServe(":8081", nil)
+}
+
+
+
+
+
+
+func graphqlServer(){
+	
+}
+
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+}
+
+
+
+
+func wsHandler(w http.ResponseWriter, r * http.Request){
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Panicln("ws upgrade failed: ", err)
+		return 
+	}
+
+	defer conn.Close()
+
+	clientsMu.Lock()
+	clients[conn] = true
+	clientsMu.Unlock()
+
+	for {
+		_, _, err := conn.ReadMessage()
+		if err != nil {
+			clientsMu.Lock()
+			delete(clients, conn)
+			clientsMu.Unlock()
+			break
+		}
+	}
+
+}
+
+func broadcastPosts(){
+	for post := range broadcast{
+		clientsMu.Lock()
+		for client := range clients{
+			err := client.WriteJSON(post)
+			if err != nil {
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+	clientsMu.Unlock()
+}
+
+
+
+func grpcServer(){
+
+}
+
+
+func wsServer(){
+	http.HandleFunc("/ws", wsHandler)
+	go http.ListenAndServe(":8083", nil)
+	go broadcastPosts()
+}
+
 func  main() {
 	// REST
 	restServer()
 	
 	// GRPC
+	grpcServer()
 
 
 	// SOAP
-
+	soapServer()
 
 	// GRAPHQL
 
-	// WEBSOCKET
+	graphqlServer()
 
-	fmt.Println("Server running:\n\tREST(:8080)")
+	// WEBSOCKET
+	wsServer()
+
+	fmt.Println("Server running:\n\tREST(:8080)\n\tSOAP(:8081)\n\tGraphQL(:8082)\n\tWebsocket(:8083)\n\tgRPC(:50051)")
 
 	select{}
 
